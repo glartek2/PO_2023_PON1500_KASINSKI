cff99ad5d305b34364835d5973e7e41f6ed45259
glartek2 <60228656+glartek2@users.noreply.github.com>
2023-10-15 20:23:57 +0200

    Update: parse method in class OptionsParser

--- oolab/src/main/java/agh/ics/oop/OptionsParser.java
+++ oolab/src/main/java/agh/ics/oop/OptionsParser.java
@@ -3,8 +3,27 @@ package agh.ics.oop;
 import agh.ics.oop.model.MoveDirection;
 
 public class OptionsParser {
-    public static MoveDirection parse(String[] args) {
+    public static MoveDirection[] parse(String[] args) {
+        MoveDirection[] directions = new MoveDirection[args.length];
 
-        return null;
+        for (int i = 0; i < args.length; i++) {
+            switch (args[i]) {
+                case "f":
+                    directions[i] = MoveDirection.FORWARD;
+                    break;
+                case "b":
+                    directions[i] = MoveDirection.BACKWARD;
+                    break;
+                case "l":
+                    directions[i] = MoveDirection.LEFT;
+                    break;
+                case "r":
+                    directions[i] = MoveDirection.RIGHT;
+                    break;
+                default:
+                    continue;
+            }
+        }
+        return directions;
     }
 }
--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -17,19 +17,19 @@ public class World {
         String[] arguments = input.split(" ");
 
         System.out.print("System wystartował\n");
-        MoveDirection directions = OptionsParser.parse(arguments);
-        run(arguments);
+        MoveDirection[] directions = OptionsParser.parse(arguments);
+        run(directions);
         System.out.print("System zakończył działanie\n");
     }
 
 
-    public static void run(String[] args) {
+    public static void run(MoveDirection[] args) {
         for (int i = 0; i < args.length; i++) {
             switch (args[i]) {
-                case "f" ->  System.out.println("Zwierzak idzie do przodu");
-                case "b" ->  System.out.println("Zwierzak idzie do tyłu");
-                case "l" -> System.out.print("Zwierzak skręca w lewo");
-                case "r" -> System.out.print("Zwierzak skręca w prawo");
+                case FORWARD ->  System.out.println("Zwierzak idzie do przodu");
+                case BACKWARD ->  System.out.println("Zwierzak idzie do tyłu");
+                case LEFT -> System.out.print("Zwierzak skręca w lewo");
+                case RIGHT -> System.out.print("Zwierzak skręca w prawo");
             }
             System.out.print(" \n");
         }
