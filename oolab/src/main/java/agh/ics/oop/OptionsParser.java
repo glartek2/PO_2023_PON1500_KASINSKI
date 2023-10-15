4a66b09418c6e2cecc184e421d2ad7376991ae40
glartek2 <60228656+glartek2@users.noreply.github.com>
2023-10-15 19:28:45 +0200

    Add: Enum MoveDirection and class OptionsParser

---
+++ oolab/src/main/java/agh/ics/oop/OptionsParser.java
@@ -0,0 +1,10 @@
+package agh.ics.oop;
+
+import agh.ics.oop.model.MoveDirection;
+
+public class OptionsParser {
+    public static MoveDirection parse(String[] args) {
+
+        return null;
+    }
+}
--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -1,16 +1,39 @@
 package agh.ics.oop;
 
+import agh.ics.oop.model.MoveDirection;
+
+import java.util.Scanner;
+
+
 public class World {
 
     public static void main(String[] args) {
-        String direction = "a";
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter arguments separated by spaces (press Enter to finish): ");
+        String input = scanner.nextLine();
+
+        String[] arguments = input.split(" ");
+
         System.out.print("System wystartował\n");
-        run(direction);
-        System.out.print("System zakończył działanie");
+        MoveDirection directions = OptionsParser.parse(arguments);
+        run(arguments);
+        System.out.print("System zakończył działanie\n");
     }
 
 
-    public static void run(String direction) {
-        System.out.print("Zwierzak idzie do przodu\n");
+    public static void run(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            switch (args[i]) {
+                case "f" ->  System.out.println("Zwierzak idzie do przodu");
+                case "b" ->  System.out.println("Zwierzak idzie do tyłu");
+                case "l" -> System.out.print("Zwierzak skręca w lewo");
+                case "r" -> System.out.print("Zwierzak skręca w prawo");
+            }
+            System.out.print(" \n");
+        }
     }
 }
+
+
